235e0cde7e9c7c49ed1efbec01b5e6a8af5e5e77
Ryan Hall <dillbc@protonmail.com>
2023-08-17 13:21:52 -0400

    completed upon review

--- src/routes/productsRouter.ts
+++ src/routes/productsRouter.ts
@@ -19,9 +19,9 @@ productsRouter.get("/products", async (req, res) => {
     let includes: string | null = (req.query.includes as string) || null;
     const query: any = {
       ...(maxPrice ? { price: { $lte: maxPrice } } : {}),
-      ...(includes ? { product: new RegExp(`${includes}`, "i") } : {}),
+      ...(includes ? { name: new RegExp(includes, "i") } : {}),
     };
-    let limit: number | null = parseInt(req.query.limit as string);
+    let limit: number | null = Number(req.query.limit);
     if (isNaN(limit)) limit = null;
     const client = await getClient();
     const cursor = client.db().collection<Product>("products").find(query);
@@ -33,23 +33,60 @@ productsRouter.get("/products", async (req, res) => {
   }
 });
 
+// productsRouter.get("/products", async (req, res) => {
+//   try {
+//     let maxPrice: number | null = Number(req.query["max-price"])
+//     if(isNaN(maxPrice)) maxPrice = null;
+//     let includes: string | null = (req.query.includes as string) || null;
+//     let limit: number | null = Number(req.query.limit);
+//     if(isNaN(limit)) limit = null;
+//     const query: any = {
+//       ...(maxPrice ? {price: {$lte: maxPrice}} : {}),
+//       ...(includes ? {name: new RegExp(`${includes}`, "i")} : {})
+//     }
+//     const client = await getClient();
+//     const cursor = client.db().collection<Product>("products").find(query);
+//     if (limit) cursor.limit(limit);
+//     const results = cursor.toArray();
+//     res.status(200).json(results)
+//   } catch(error) {
+//     errorResponse(error, res)
+//   }
+// })
+
 productsRouter.get("/products/:id", async (req, res) => {
   try {
     const _id: ObjectId = new ObjectId(req.params.id);
     const client = await getClient();
-    const result = await client
-      .db()
-      .collection<Product>("products")
-      .findOne({ _id });
+    const result = await client.db().collection<Product>("products");
+    client.db().collection<Product>("products").findOne({ _id });
     if (result) {
       res.status(200).json(result);
     } else {
-      res.status(404).json({ message: `Item with id:${_id} not found` });
+      res.status(404).json({ message: `Product with id:${_id} not found` });
     }
   } catch (error) {
     errorResponse(error, res);
   }
 });
+
+// productsRouter.get("/products/:id", async (req, res) => {
+//   try {
+//     const _id: ObjectId = new ObjectId(req.params.id);
+//     const client = await getClient();
+//     const result = await client
+//       .db()
+//       .collection<Product>("products")
+//       .findOne({ _id });
+//     if (result) {
+//       res.status(200).json(result);
+//     } else {
+//       res.status(404).json({ message: `Item with id:${_id} not found` });
+//     }
+//   } catch (error) {
+//     errorResponse(error, res);
+//   }
+// });
 
 productsRouter.post("/products", async (req, res) => {
   try {
@@ -74,7 +111,44 @@ productsRouter.put("/products/:id", async (req, res) => {
     if (result.matchedCount) {
       res.status(200).json(updatedProduct);
     } else {
-      res.status(404).json({ message: `Product not found` });
+      res.status(404).json({ message: `Product id:${_id} not found` });
+    }
+  } catch (error) {
+    errorResponse(error, res);
+  }
+});
+
+// productsRouter.put("/products/:id", async (req, res) => {
+//   try {
+//     const _id: ObjectId = new ObjectId(req.params.id);
+//     const updatedProduct: Product = req.body;
+//     const client = await getClient();
+//     const result = await client
+//       .db()
+//       .collection<Product>("products")
+//       .replaceOne({ _id }, updatedProduct);
+//     if (result.matchedCount) {
+//       res.status(200).json(updatedProduct);
+//     } else {
+//       res.status(404).json({ message: `Product not found` });
+//     }
+//   } catch (error) {
+//     errorResponse(error, res);
+//   }
+// });
+
+productsRouter.delete("/products/:id", async (req, res) => {
+  try {
+    const _id: ObjectId = new ObjectId(req.params.id);
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<Product>("products")
+      .deleteOne({ _id });
+    if (result.deletedCount) {
+      res.sendStatus(204).json();
+    } else {
+      res.status(404).json({ message: `Product id:${_id} not found` });
     }
   } catch (error) {
     errorResponse(error, res);
--- src/routes/usersRouter.ts
+++ src/routes/usersRouter.ts
@@ -2,6 +2,9 @@
 
 import express from "express";
 import { getClient } from "../db";
+import { ObjectId } from "mongodb";
+import User from "../models/User";
+
 // import User from "../models/User";
 const usersRouter = express.Router();
 const errorResponse = (error: any, res: any) => {
@@ -11,5 +14,68 @@ const errorResponse = (error: any, res: any) => {
 };
 
 // endpoints go here
+
+usersRouter.get("/users/:id", async (req, res) => {
+  try {
+    const _id: ObjectId = new ObjectId(req.params.id);
+    const client = await getClient();
+    const result = await client.db().collection<User>("users").findOne({ _id });
+    if (result) {
+      res.status(200).json(result);
+    } else {
+      res.status(404).json({ message: `User with id:${_id} not found` });
+    }
+  } catch (error) {
+    errorResponse(error, res);
+  }
+});
+
+usersRouter.post("/users", async (req, res) => {
+  try {
+    const newUser: User = req.body;
+    const client = await getClient();
+    client.db().collection<User>("users").insertOne(newUser);
+    res.status(201).json(newUser);
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+usersRouter.put("/users/:id", async (req, res) => {
+  try {
+    const _id: ObjectId = new ObjectId(req.params.id);
+    const updatedUser: User = req.body;
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<User>("users")
+      .replaceOne({ _id }, updatedUser);
+    if (result.modifiedCount) {
+      res.status(200).json(updatedUser);
+    } else {
+      res.status(404).json({ message: `User with id:${_id} not found` });
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
+
+usersRouter.delete("/users/:id", async (req, res) => {
+  try {
+    const _id: ObjectId = new ObjectId(req.params.id);
+    const client = await getClient();
+    const result = await client
+      .db()
+      .collection<User>("users")
+      .deleteOne({ _id });
+    if (result.deletedCount) {
+      res.sendStatus(204);
+    } else {
+      res.status(404).json({ message: "nah" });
+    }
+  } catch (err) {
+    errorResponse(err, res);
+  }
+});
 
 export default usersRouter;
